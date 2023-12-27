421d917ef7f123a7529c405614ea07b7502d11c7
yash6912 <122801779+yash6912@users.noreply.github.com>
2023-12-27 12:57:36 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,41 @@
-import React, { useState } from 'react';
-import { Button } from 'antd';
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
-const Counter = () => {
- const [count, setCount] = useState(0);
+import logo from "./assets/instagram.png"
+import logo1 from "./assets/google_play_logo.png"
+import logo2 from "./assets/microsoft_logo.png"
 
- const incrementCounter = () => {
-    setCount(count + 1);
- };
+function App() {
+  const [login,setlogin]=useState(true)
 
- const decrementCounter = () => {
-    setCount(count - 1);
- };
+  const loginSwitch = () => {
+    setlogin(!login)
+  }
+  return (
+   <div id='Container'>
+        <div id="user">
+              <img src={logo} alt="" />
+              <input hidden={login} type="text" name="" placeholder='Mobile Number or Email' id="" />
+              <input hidden={login} type="text" name="" placeholder='Full Name' id="" />
+              <input type="email" name="" id="" placeholder='Phone number, username, or email'/>
+              <input type="text" name="" id="" placeholder='Password' />
+              <button>{login?"Sign in":"Sign up"}</button>
+              <div id="OR"><p>______OR_______</p></div>
+              <div id="user1">
+              <a href='#'>Login with Facebook?</a>
+              <a href='#'><p>Forgot password?</p></a>
+              </div>
+          <div id='footer'>
+            {login?"Don't have account?":"have an account?"} <span onClick={loginSwitch}>{login?"Sign up":"Log in"}</span>
+            <div><p>Get the App</p></div>
+            <div id="footer-image">              
+            <img src={logo1} alt="" id='gl' />
+            <img src={logo2} alt="" id='ml' />
+            </div>                       
+          </div>         
+          </div>            
+   </div>
+  );
+}
 
- return (
-    <div id= "container">
-      <div id="counter">
-      <h1>Counter: {count}</h1>
-      <div id="button">
-      <Button onClick={incrementCounter}>Increment</Button>
-      <Button onClick={decrementCounter}>Decrement</Button>
-      </div>
-      </div>
-      
-      
-    </div>
- );
-};
-
-export default Counter;
+export default App;
